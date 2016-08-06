e8db4e48bbfe0baacd1c29377afe1a53877fd928
Andrey Polischuk <andre.polischuk@gmail.com>
2016-08-06 15:24:51 +0300

    Some tweaks

--- index.js
+++ index.js
@@ -12,82 +12,80 @@ const cyan = '#0184bc';
 const white = '#a0a1a7';
 const lightBlack = '#696c77';
 
-exports.decorateConfig = config => {
-  return Object.assign({}, config, {
-    backgroundColor,
-    foregroundColor,
-    borderColor: backgroundColor,
-    cursorColor: foregroundColor,
-    colors: {
-      black: backgroundColor,
-      red,
-      green,
-      yellow,
-      blue,
-      magenta,
-      cyan,
-      white,
-      lightBlack,
-      lightRed: red,
-      lightGreen: green,
-      lightYellow: yellow,
-      lightBlue: blue,
-      lightMagenta: magenta,
-      lightCyan: cyan,
-      lightWhite: foregroundColor
-    },
-    css: `
-      ${config.css || ''}
-      .header_header {
-        top: 0;
-        right: 0;
-        left: 0;
-      }
-      .tabs_list {
-        background-color: ${darkBackgroundColor} !important;
-        border-bottom-color: rgba(0, 0, 0, .15) !important;
-      }
-      .tab_tab {
-        color: ${white} !important;
-      }
-      .tab_tab.tab_active {
-        font-weight: 500;
-        background-color: ${backgroundColor};
-        border-color: rgba(0, 0, 0, .25) !important;
-      }
-      .tab_tab.tab_active::before {
-        border-bottom-color: ${backgroundColor};
-      }
-      .tab_tab::after {
-        content: "";
-        position: absolute;
-        pointer-events: none;
-        z-index: 2;
-        top: 0px;
-        left: -1px;
-        bottom: -1px;
-        width: 2px;
-        border-radius: 0;
-        background: ${darkBlue};
-        opacity: 0;
-        transition: opacity .16s;
-      }
-      .tab_tab.tab_active::after {
-        opacity: 1;
-        transition-duration: .32s;
-      }
-      .tabs_title, .tab_icon, .tab_tab.tab_active {
-        color: ${lightBlack} !important;
-      }
-      .tab_tab.tab_hasActivity {
-        color: ${blue} !important;
-      }
-    `,
-    termCSS: `
-      ${config.termCSS || ''}
-      .cursor-node {
-        mix-blend-mode: difference;
-      }
-    `
-  });
-};
+exports.decorateConfig = config => Object.assign({}, config, {
+  backgroundColor,
+  foregroundColor,
+  borderColor: backgroundColor,
+  cursorColor: foregroundColor,
+  colors: {
+    black: backgroundColor,
+    red,
+    green,
+    yellow,
+    blue,
+    magenta,
+    cyan,
+    white,
+    lightBlack,
+    lightRed: red,
+    lightGreen: green,
+    lightYellow: yellow,
+    lightBlue: blue,
+    lightMagenta: magenta,
+    lightCyan: cyan,
+    lightWhite: foregroundColor
+  },
+  css: `
+    ${config.css || ''}
+    .header_header {
+      top: 0;
+      right: 0;
+      left: 0;
+    }
+    .tabs_list {
+      background-color: ${darkBackgroundColor} !important;
+      border-bottom-color: rgba(0, 0, 0, .15) !important;
+    }
+    .tab_tab {
+      color: ${white} !important;
+    }
+    .tab_tab.tab_active {
+      font-weight: 500;
+      background-color: ${backgroundColor};
+      border-color: rgba(0, 0, 0, .25) !important;
+    }
+    .tab_tab.tab_active::before {
+      border-bottom-color: ${backgroundColor};
+    }
+    .tab_tab::after {
+      content: "";
+      position: absolute;
+      pointer-events: none;
+      z-index: 2;
+      top: 0px;
+      left: -1px;
+      bottom: -1px;
+      width: 2px;
+      border-radius: 0;
+      background: ${darkBlue};
+      opacity: 0;
+      transition: opacity .16s;
+    }
+    .tab_tab.tab_active::after {
+      opacity: 1;
+      transition-duration: .32s;
+    }
+    .tabs_title, .tab_icon, .tab_tab.tab_active {
+      color: ${lightBlack} !important;
+    }
+    .tab_tab.tab_hasActivity {
+      color: ${blue} !important;
+    }
+  `,
+  termCSS: `
+    ${config.termCSS || ''}
+    .cursor-node {
+      mix-blend-mode: difference;
+    }
+  `
+});
